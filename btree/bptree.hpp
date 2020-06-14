6caac777b880dfac733239ebe85c59bc3024f256
wjchenqing <wjchenqing@sjtu.edu.cn>
2020-06-14 19:05:58 +0800

    Try, try your way.....

--- btree/bptree.hpp
+++ btree/bptree.hpp
@@ -124,7 +124,7 @@ namespace sjtu {
         }
 
         void write_all(){
-            if (num < 500)
+            if (num < 300)
                 return;
             while (num > 0){
                 _write(&pool[occupied[num--]]);
